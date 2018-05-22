d8e2fd7d0b7fef96b4e25dea3c9ee0e2ec3ee741
Yasin Sukru TAN <yasin@tan.com>
2018-05-22 17:56:54 +0300

    First commit

---
+++ src/main/java/com/fishmarket/gateway/GatewayApplication.java
@@ -0,0 +1,37 @@
+package com.fishmarket.gateway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
+import org.springframework.context.annotation.Bean;
+
+import com.fishmarket.gateway.filters.ErrorFilter;
+import com.fishmarket.gateway.filters.PostFilter;
+import com.fishmarket.gateway.filters.PreFilter;
+import com.fishmarket.gateway.filters.RouteFilter;
+
+@SpringBootApplication
+@EnableZuulProxy
+public class GatewayApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GatewayApplication.class, args);
+	}
+	
+	@Bean
+    public PreFilter preFilter() {
+        return new PreFilter();
+    }
+    @Bean
+    public PostFilter postFilter() {
+        return new PostFilter();
+    }
+    @Bean
+    public ErrorFilter errorFilter() {
+        return new ErrorFilter();
+    }
+    @Bean
+    public RouteFilter routeFilter() {
+        return new RouteFilter();
+    }
+}
